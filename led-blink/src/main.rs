273ba571a282006963dab90f82b1a81defef5ecc
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-29 18:21:33 +0900

    define Button1

--- led-blink/src/main.rs
+++ led-blink/src/main.rs
@@ -1,15 +1,45 @@
 #![no_std]
 #![no_main]
+#![allow(dead_code)]
 
 use panic_halt as _;
-use support::*;
+use wio_terminal as wio;
+
+use wio::entry;
+use wio::hal::gpio::{Floating, Input, Output, Pc26, Port, PushPull};
+use wio::pac::Peripherals;
+use wio::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    let (mut user_led, mut delay) = support::init();
+    let peripherals = Peripherals::take().unwrap();
+    let mut pins = wio::Pins::new(peripherals.PORT);
+    let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
+    let button1 = Button1::new(pins.button1, &mut pins.port);
 
     loop {
-        delay.delay_ms(100u16);
-        user_led.toggle();
+        if button1.is_pressed() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+    }
+}
+
+struct Button1 {
+    pin: Pc26<Input<Floating>>,
+}
+impl Button1 {
+    fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> Button1 {
+        Button1 {
+            pin: pin.into_floating_input(port),
+        }
+    }
+
+    fn is_pressed(&self) -> bool {
+        self.pin.is_low().unwrap()
+    }
+    fn is_released(&self) -> bool {
+        self.pin.is_high().unwrap()
     }
 }
