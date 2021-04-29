7f7504d3528c9de665bd0a026aa29a9a33c5f39d
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-29 18:45:12 +0900

    define BlueLed struct

--- led-blink/src/main.rs
+++ led-blink/src/main.rs
@@ -6,7 +6,7 @@ use panic_halt as _;
 use wio_terminal as wio;
 
 use wio::entry;
-use wio::hal::gpio::{Floating, Input, Output, Pc26, Port, PushPull};
+use wio::hal::gpio::{Floating, Input, Output, Pa15, Pc26, Port, PushPull};
 use wio::pac::Peripherals;
 use wio::prelude::*;
 
@@ -14,24 +14,24 @@ use wio::prelude::*;
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     let mut pins = wio::Pins::new(peripherals.PORT);
-    let mut led = pins.user_led.into_push_pull_output(&mut pins.port);
-    let button1 = Button1::new(pins.button1, &mut pins.port);
+    let mut led = BlueLed::new(pins.user_led, &mut pins.port);
+    let button1 = RightButton::new(pins.button1, &mut pins.port);
 
     loop {
         if button1.is_pressed() {
-            led.set_high().unwrap();
+            led.turn_on();
         } else {
-            led.set_low().unwrap();
+            led.turn_off();
         }
     }
 }
 
-struct Button1 {
+struct RightButton {
     pin: Pc26<Input<Floating>>,
 }
-impl Button1 {
-    fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> Button1 {
-        Button1 {
+impl RightButton {
+    fn new(pin: Pc26<Input<Floating>>, port: &mut Port) -> RightButton {
+        RightButton {
             pin: pin.into_floating_input(port),
         }
     }
@@ -41,5 +41,23 @@ impl Button1 {
     }
     fn is_released(&self) -> bool {
         self.pin.is_high().unwrap()
+    }
+}
+
+struct BlueLed {
+    pin: Pa15<Output<PushPull>>,
+}
+impl BlueLed {
+    fn new(pin: Pa15<Input<Floating>>, port: &mut Port) -> BlueLed {
+        BlueLed {
+            pin: pin.into_push_pull_output(port),
+        }
+    }
+
+    fn turn_on(&mut self) {
+        self.pin.set_high().unwrap();
+    }
+    fn turn_off(&mut self) {
+        self.pin.set_low().unwrap();
     }
 }
